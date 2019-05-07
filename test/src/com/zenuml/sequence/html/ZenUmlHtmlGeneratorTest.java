ae52fb30d577192c48daf7024514ccd9b7a3e6e0
PengXiao <eagle.xiao@gmail.com>
2019-05-07 21:39:40 +1000

    geneate full html

--- test/src/com/zenuml/sequence/html/ZenUmlHtmlGeneratorTest.java
+++ test/src/com/zenuml/sequence/html/ZenUmlHtmlGeneratorTest.java
@@ -9,6 +9,8 @@ public class ZenUmlHtmlGeneratorTest {
     /**
      * 1. generate an empty html with head and body
      * 2. show an hard coded sequence diagram
+     *   2.1 add main(#demo)->div(#diagram)->seq-diagram
+     *   2.2 add script (vue-sequence-bundle.js)
      * 3. accept DSL as dynamic content
      */
 
@@ -16,6 +18,7 @@ public class ZenUmlHtmlGeneratorTest {
     public void test_should_generate_an_empty_html() {
         ZenUmlHtmlGenerator generator = new ZenUmlHtmlGenerator();
 
-        assertEquals("<html><head></head><body></body></html>", generator.from(""));
+        String expected = "<html><head></head><body></body></html>";
+        assertEquals(expected, generator.from(""));
     }
 }
