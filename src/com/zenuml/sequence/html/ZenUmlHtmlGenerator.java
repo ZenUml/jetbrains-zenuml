16fae29e374ed63a402585db107eb55c67034050
PengXiao <eagle.xiao@gmail.com>
2019-05-10 19:09:55 +1000

    Added <seq-diagram> element.

--- src/com/zenuml/sequence/html/ZenUmlHtmlGenerator.java
+++ src/com/zenuml/sequence/html/ZenUmlHtmlGenerator.java
@@ -2,6 +2,6 @@ package com.zenuml.sequence.html;
 
 public class ZenUmlHtmlGenerator {
     public String from(String dsl) {
-        return "<html><head></head><body></body></html>";
+        return "<html><head></head><body><main id=\"domo\"><seq-diagram></seq-diagram></main></body></html>";
     }
 }
--- test/src/com/zenuml/sequence/html/ZenUmlHtmlGeneratorTest.java
+++ test/src/com/zenuml/sequence/html/ZenUmlHtmlGeneratorTest.java
@@ -9,7 +9,7 @@ public class ZenUmlHtmlGeneratorTest {
     /**
      * 1. generate an empty html with head and body
      * 2. show an hard coded sequence diagram
-     *   2.1 add main(#demo)->div(#diagram)->seq-diagram
+     *   2.1 add main(#demo)->seq-diagram
      *   2.2 add script (vue-sequence-bundle.js)
      * 3. accept DSL as dynamic content
      */
@@ -18,7 +18,7 @@ public class ZenUmlHtmlGeneratorTest {
     public void test_should_generate_an_empty_html() {
         ZenUmlHtmlGenerator generator = new ZenUmlHtmlGenerator();
 
-        String expected = "<html><head></head><body></body></html>";
+        String expected = "<html><head></head><body><main id=\"domo\"><seq-diagram></seq-diagram></main></body></html>";
         assertEquals(expected, generator.from(""));
     }
 }
