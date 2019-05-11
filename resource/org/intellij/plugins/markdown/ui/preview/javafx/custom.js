0d13341c8aa336c0f30d20db79e8dac0abfbee9b
PengXiao <eagle.xiao@gmail.com>
2019-05-11 13:20:46 +1000

    Removed inlined js from html.

--- resource/org/intellij/plugins/markdown/ui/preview/javafx/custom.js
+++ resource/org/intellij/plugins/markdown/ui/preview/javafx/custom.js
@@ -3,5 +3,6 @@
   setTimeout(function () {
     // the DOM will be available here
     var app = document.getElementById("app2"); app.innerHTML='replaced with external custom JS';
+
   }, 5000)
 })();
