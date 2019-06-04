a4108f6ca938ce30d866b2cdabfccb5ccdb1b738
Yanhui Li <yanhui.li@fabricgroup.com.au>
2019-06-04 21:25:33 +1000

    Learn ZenUML tool window spike

---
+++ src/com/zenuml/sequence/ui/LearnZenUMLToolWindowFactory.java
@@ -0,0 +1,18 @@
+package com.zenuml.sequence.ui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class LearnZenUMLToolWindowFactory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
---
+++ src/com/zenuml/sequence/ui/MyToolWindow.java
@@ -0,0 +1,52 @@
+package com.zenuml.sequence.ui;
+
+import com.intellij.openapi.wm.ToolWindow;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+public class MyToolWindow {
+    private JButton refreshToolWindowButton;
+    private JButton hideToolWindowButton;
+    private JLabel currentDate;
+    private JLabel currentTime;
+    private JLabel timeZone;
+    private JPanel myToolWindowContent;
+
+    public MyToolWindow(ToolWindow toolWindow) {
+        hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
+        refreshToolWindowButton.addActionListener(e -> currentDateTime());
+
+        this.currentDateTime();
+    }
+
+
+    public void currentDateTime() {
+        // Get current date and time
+        Calendar instance = Calendar.getInstance();
+        currentDate.setText(String.valueOf(instance.get(Calendar.DAY_OF_MONTH)) + "/"
+                + String.valueOf(instance.get(Calendar.MONTH) + 1) + "/" +
+                String.valueOf(instance.get(Calendar.YEAR)));
+        int min = instance.get(Calendar.MINUTE);
+        String strMin;
+        if (min < 10) {
+            strMin = "0" + String.valueOf(min);
+        } else {
+            strMin = String.valueOf(min);
+        }
+        currentTime.setText(instance.get(Calendar.HOUR_OF_DAY) + ":" + strMin);
+        // Get time zone
+        long gmt_Offset = instance.get(Calendar.ZONE_OFFSET); // offset from GMT in milliseconds
+        String str_gmt_Offset = String.valueOf(gmt_Offset / 3600000);
+        str_gmt_Offset = (gmt_Offset > 0) ? "GMT + " + str_gmt_Offset : "GMT - " + str_gmt_Offset;
+        timeZone.setText(str_gmt_Offset);
+
+
+    }
+
+    public JPanel getContent() {
+        return myToolWindowContent;
+    }
+}
