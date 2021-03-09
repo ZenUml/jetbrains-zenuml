d3e3618f0085220f0b2a535673a1f5bcdc3defc9
MrCoder <eagle.xiao@gmail.com>
2021-03-09 18:13:52 +1100

    Updated IC dependency to 2020.3.2.

--- test/src/org/intellij/plugins/markdown/MarkdownFileTypeTest.java
+++ test/src/org/intellij/plugins/markdown/MarkdownFileTypeTest.java
@@ -3,14 +3,14 @@ package org.intellij.plugins.markdown;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
-import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.HeavyPlatformTestCase;
 import org.intellij.plugins.markdown.lang.MarkdownFileType;
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownFile;
 
 import java.io.File;
 import java.io.IOException;
 
-public class MarkdownFileTypeTest extends PlatformTestCase {
+public class MarkdownFileTypeTest extends HeavyPlatformTestCase {
   public void testZExtension() throws IOException {
     doTest(".z");
   }
@@ -26,10 +26,7 @@ public class MarkdownFileTypeTest extends PlatformTestCase {
   }
 
   private void doTest(String extension) throws IOException {
-    File dir = createTempDirectory();
-    File file = FileUtil.createTempFile(dir, "test", extension, true);
-    VirtualFile virtualFile = getVirtualFile(file);
-    assertNotNull(virtualFile);
+    VirtualFile virtualFile = getTempDir().createVirtualFile(extension);
     PsiFile psi = getPsiManager().findFile(virtualFile);
     assertTrue(psi instanceof MarkdownFile);
     assertEquals(MarkdownFileType.INSTANCE, virtualFile.getFileType());
