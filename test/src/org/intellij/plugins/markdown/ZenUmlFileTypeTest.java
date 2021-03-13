6d02e716ea2c0a5feae26138ce1ae8746bcb364a
MrCoder <eagle.xiao@gmail.com>
2021-03-13 21:55:24 +1100

    Added FileType test back.

--- test/src/org/intellij/plugins/markdown/ZenUmlFileTypeTest.java
+++ test/src/org/intellij/plugins/markdown/ZenUmlFileTypeTest.java
@@ -6,12 +6,12 @@ import com.intellij.testFramework.HeavyPlatformTestCase;
 import com.intellij.testFramework.SkipSlowTestLocally;
 import com.intellij.testFramework.fixtures.SkipWithExecutionPolicy;
 import org.intellij.plugins.markdown.lang.ZenUmlFileType;
+import org.intellij.plugins.markdown.lang.ZenUmlLanguage;
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownFile;
 import org.junit.Ignore;
 
 import java.io.IOException;
 
-@Ignore("Failing due to ZenUmlLanguage is initialised multiple times in different class loaders")
 public class ZenUmlFileTypeTest extends HeavyPlatformTestCase {
   public void testZExtension() throws IOException {
     doTest(".z");
@@ -23,14 +23,10 @@ public class ZenUmlFileTypeTest extends HeavyPlatformTestCase {
     doTest(".zenuml");
   }
 
-  public void testMdExtension() throws IOException {
-    doTest(".zen");
-  }
-
   private void doTest(String extension) throws IOException {
     VirtualFile virtualFile = getTempDir().createVirtualFile(extension);
     PsiFile psi = getPsiManager().findFile(virtualFile);
-    assertTrue(psi instanceof MarkdownFile);
-    assertEquals(ZenUmlFileType.INSTANCE, virtualFile.getFileType());
+    assertEquals("ZenUML", psi.getLanguage().getID());
+    assertEquals("ZenUML", virtualFile.getFileType().getName());
   }
 }
