bc09bccd4d56c47279b7be4f151b46b36b731969
MrCoder <eagle.xiao@gmail.com>
2022-02-22 08:42:21 +1100

    Updated vue-sequence and vue-gtag.
    Note that due to checkProtocolTask, gtag is not working.

--- jetbrains-viewer/src/main.ts
+++ jetbrains-viewer/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueGtag from 'vue-gtag';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'vue-sequence/dist/vue-sequence.css'
+
+Vue.use(VueGtag, {
+  config: {
+    id: 'UA-114996686-1'
+  }
+})
 
 Vue.config.productionTip = false
 
