27f74a37643172d8131372d8a65930c2913437cb
MrCoder <eagle.xiao@gmail.com>
2022-02-25 14:45:47 +1100

    Updated vue-sequence. Fixed an issue in Creation component.

--- jetbrains-viewer/src/App.vue
+++ jetbrains-viewer/src/App.vue
@@ -15,7 +15,7 @@ const DiagramFrame = VueSequence.DiagramFrame;
     DiagramFrame
   },
 })
-export default class Home extends Vue {}
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
