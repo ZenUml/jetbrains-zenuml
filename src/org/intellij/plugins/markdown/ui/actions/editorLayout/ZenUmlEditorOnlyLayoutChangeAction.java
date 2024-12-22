6228d0c595708d2d6a6f9662af7bd7c25c887799
MrCoder <eagle.xiao@gmail.com>
2024-12-22 21:26:57 +1100

    perf: ensure actions are updated on EDT thread
    - Update BaseChangeSplitLayoutAction, ZenUmlEditorOnlyLayoutChangeAction, and ShowSequenceAction to specify EDT thread for action updates
    - Add getActionUpdateThread() method to each action class, returning ActionUpdateThread.EDT
    - This change ensures that action updates are performed on the Event Dispatch Thread, improving performance and avoiding potential threading issues

--- src/org/intellij/plugins/markdown/ui/actions/editorLayout/ZenUmlEditorOnlyLayoutChangeAction.java
+++ src/org/intellij/plugins/markdown/ui/actions/editorLayout/ZenUmlEditorOnlyLayoutChangeAction.java
@@ -1,9 +1,17 @@
 package org.intellij.plugins.markdown.ui.actions.editorLayout;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import org.intellij.plugins.markdown.ui.split.SplitFileEditor;
+import org.jetbrains.annotations.NotNull;
 
 public class ZenUmlEditorOnlyLayoutChangeAction extends BaseChangeSplitLayoutAction {
   protected ZenUmlEditorOnlyLayoutChangeAction() {
     super(SplitFileEditor.SplitEditorLayout.FIRST);
+  }
+
+  @Override
+  @NotNull
+  public ActionUpdateThread getActionUpdateThread() {
+    return ActionUpdateThread.EDT;
   }
 }
