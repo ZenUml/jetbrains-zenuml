c9f5cbb6a58dbd16f8ae964c07b7b0c4ab90b094
Yanhui Li <whimet@gmail.com>
2024-12-26 22:46:38 +1100

    Intellij Platform Plugin migration - fix the verification check

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ plugins {
   id("org.jetbrains.kotlin.jvm") version "2.0.0-Beta2"
   // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
   id("org.jetbrains.intellij.platform") version "2.2.0"
-  id("org.jetbrains.intellij.platform.migration") version "2.2.0"
   // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
   id("org.jetbrains.changelog") version "1.3.1"
   // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
@@ -93,16 +92,6 @@ intellijPlatform {
     channels = listOf(if ("true" == System.getenv("PRE_RELEASE")) "EAP" else "default")
   }
 
-//  pluginName.set(properties("pluginName"))
-//  version.set(properties("platformVersion"))
-//  type.set(properties("platformType"))
-//  downloadSources.set(properties("platformDownloadSources").toBoolean())
-//  updateSinceUntilBuild.set(false) // don't write information of current IntelliJ build into plugin.xml, instead use information from patchPluginXml
-
-//  Plugin Dependencies:
-//  https://www.jetbrains.org/intellij/sdk/docs/basics/plugin_structure/plugin_dependencies.html
-//
-//  plugins.set(listOf("com.intellij.java", "org.intellij.intelliLang"))
 }
 
 // Configure detekt plugin.
@@ -169,44 +158,6 @@ tasks {
   withType<Detekt> {
     jvmTarget = "11"
   }
-//  patchPluginXml {
-//    version.set(properties("pluginVersion"))
-//    sinceBuild.set((properties("pluginSinceBuild")))
-//    // untilBuild(pluginUntilBuild) --> don't set "untilBuild" to allow new versions to use existing plugin without changes until breaking API changes are known
-//
-//    // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-//    pluginDescription.set(
-//      provider {
-//        File(projectDir, "README.md").readText().lines().run {
-//          val start = "<!-- Plugin description -->"
-//          val end = "<!-- Plugin description end -->"
-//
-//          if (!containsAll(listOf(start, end))) {
-//            throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-//          }
-//          subList(indexOf(start) + 1, indexOf(end))
-//        }.joinToString("\n").run { markdownToHTML(this) }
-//      }
-//    )
-//
-//    // Get the latest available change notes from the changelog file
-//    changeNotes.set(
-//      provider {
-//        changelog.getLatest().toHTML()
-//      }
-//    )
-//  }
-
-//  runPluginVerifier {
-//    ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
-//  }
-//
-//  publishPlugin {
-//    dependsOn("patchChangelog")
-//    token.set(System.getenv("PUBLISH_TOKEN"))
-//    // if release is marked as a pre-release in the GitHub release, push it to EAP
-//    channels.set(listOf(if ("true" == System.getenv("PRE_RELEASE")) "EAP" else "default"))
-//  }
   changelog {
     version.set(properties("pluginVersion"))
     header.set(provider { "[${project.version}] - ${date()}" })
